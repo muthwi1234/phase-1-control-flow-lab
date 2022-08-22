7b1d6d890e9a3dfa0ac192f9300f761f5dae1f19
Peter <peterkilungya1@gmail.com>
2022-08-22 14:27:48 +0300

    assignment completeed

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(n) {
+
+  if (n > 2500) {
+    return 'No can do.';
+  } else if (n > 2000) {
+    return 'I will gladly take your thirty bucks.';
+
+  }
+  else {
+    return "This one is on me!"
+
+  }
+}
+function ternaryCheckCity(city) {
+  let cityName = city === "NYC" ? "Ok, sounds good." : "No go.";
+  return cityName
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.'
+      
+      break;
+    case 'not as generous':
+    return 'Thank you.'
+  
+    default:
+      return 'Bye.'
+  }
 }
